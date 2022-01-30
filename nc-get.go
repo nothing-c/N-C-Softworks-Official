f1424a21fb56fc0ec5714b555eaf442dba7d2118
nothing-c <55337124+nothing-c@users.noreply.github.com>
2022-01-30 15:19:46 -0600

    Add files via upload
    Do you think wget and cURL are bloat? Do you want a bare-bones, no-frills piece of software to fetch your webpages? Then good news!
    N-C Softworks is proud to release our new "nc-get" tool! Download now, for the price of FREE!

---
+++ nc-get.go
@@ -0,0 +1,57 @@
+/*
+     ###         ###       #########  ###########     ##########   #########  #############
+    ######      ###       ###        ###             ###    ###   ###             ###
+   ###  ###    ###       ###          ##########    ###    ###   ########        ###
+  ###    ###  ###  ###  ###                   ###  ###    ###   ###             ###
+ ###      ######       ###                   ###  ###    ###   ###             ###
+###        ###        ###########  ###########   ##########   ###             ###
+
+*/
+
+
+/*
+N-C SOFTWORKS NC-GET v1.0
+Lightweight version of wget/curl
+Usage: crapget [url]
+*/
+package main;
+
+import(
+	"net/http"
+	"io/ioutil"
+	"regexp"
+	"fmt"
+	"os"
+)
+
+func checker(name string)(filename string){
+	i := 0;
+	filename = name;
+	for {
+		_, err := ioutil.ReadFile(string(filename));
+		if err != nil{
+			return filename;
+		} else {
+			//concat string and int
+			filename = fmt.Sprint(filename, i);
+		}
+		i++;
+	}
+}
+
+func main(){
+	pull, err := http.Get(os.Args[1]);
+	if err != nil {
+		fmt.Println(err);
+	} else {
+		//grab only extension - this needs work
+		regex, _ := regexp.Compile("[A-Za-z0-9]*.[A-Za-z]+");
+		//to get the name of the file
+		possible := regex.FindAllStringSubmatch(os.Args[1],-1);
+		filename := possible[len(possible) - 1];
+		realname := checker(filename[0]);
+		pulled, _ := ioutil.ReadAll(pull.Body);
+		ioutil.WriteFile(realname, []byte(pulled), 0666);
+		fmt.Println("done");
+	}
+}
