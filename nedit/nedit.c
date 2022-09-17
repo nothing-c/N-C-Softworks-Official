b48f1c99ffc01bdd7446c6110f578f2c6d40fa3e
nothing-c <55337124+nothing-c@users.noreply.github.com>
2022-09-17 17:43:38 -0500

    Update nedit.c

--- nedit/nedit.c
+++ nedit/nedit.c
@@ -18,4 +18,4 @@ typedef char* S;
 typedef FILE* F;
 #include <unistd.h>
 #define W(x) while(true){x}
-I main(I c, S v[]){I l=0;initscr();F f=fopen(v[1],"r");while(!feof(f)){I h=fgetc(f);addch(h);if(h=='\n'){l++;}};l++;W(F f=fopen(v[1], "ab+");I c=getch();fputc(c,f);if(c=='\n'){move(l++,0);}refresh();fclose(f);)return 0;}
+I main(I c, S v[]){if(c<2){puts("Specify a file");return 1;}I l=0;initscr();F f=fopen(v[1],"r");while(!feof(f)){I h=fgetc(f);addch(h);if(h=='\n'){l++;}};l++;W(F f=fopen(v[1], "ab+");I c=getch();fputc(c,f);if(c=='\n'){move(l++,0);}refresh();fclose(f);)return 0;}
